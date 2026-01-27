618f4130ddde545afec1507e775306391d1e7e5f
Baptiste Venault <provenault@gmail.com>
2026-01-27 11:32:06 +0100

    sync upstream tp03

--- tp03/3-hrsoft/HRSoftTests.cpp
+++ tp03/3-hrsoft/HRSoftTests.cpp
@@ -1,31 +1,34 @@
 #include "HRSoft/HRSoftSystem.hpp"
 
-int main()
-{
-    auto system = HRSoftSystem {};
+int main() {
+  auto system = HRSoftSystem{};
 
-    auto& rd_dpt = system.add_department("R&D");
-    auto& market_dpt = system.add_department("Marketing");
+  auto &rd_dpt = system.add_department("R&D");
+  auto &market_dpt = system.add_department("Marketing");
 
-    auto& charline = rd_dpt.add_employee("Charline", 6000, nullptr);
-    auto& jacques = rd_dpt.add_employee("Jacques", 2500, &charline);
-    auto& paul = market_dpt.add_employee("Paul", 2500, &charline);
+  auto &charline = rd_dpt.add_employee("Charline", 6000, nullptr);
+  auto &jacques = rd_dpt.add_employee("Jacques", 2500, &charline);
+  auto &paul = market_dpt.add_employee("Paul", 2500, &charline);
 
-    // TODO Q2.
-    // Affiche différentes informations du système.
-    // rd_dpt.print_employees();
-    // system.print_all_departments();
-    // system.print_all_employees();
+  // TODO Q2.
+  // Affiche différentes informations du système.
+  // rd_dpt.print_employees();
+  // system.print_all_departments();
+  // system.print_all_employees();
 
-    // TODO Q3.
-    // Affiche tous les employés managés par Charline.
-    // charline.print_subordinates();
+  // TODO Q3.
+  // Affiche tous les employés managés par Charline.
+  // charline.print_subordinates();
 
-    // TODO Q4.
-    // Jacques est licencié...
-    // system.remove_employee(jacques);
-    // rd_dpt.print_employees();
-    // charline.print_subordinates();
+  // TODO Q4.
+  // Jacques est licencié...
+  // system.remove_employee(jacques);
+  // rd_dpt.print_employees();
+  // charline.print_subordinates();
 
-    return 0;
+  // Un employée inexistant du système ne devrait pas le faire planter.
+  // Employee athanase("Athanase", 0);
+  // system.remove_employee(jacques);
+
+  return 0;
 }
